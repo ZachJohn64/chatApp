c3f416b839d84be643b228ae02fb17207b55e1ea
ZachJohn64 <zachdjohnson64@gmail.com>
2020-03-20 12:57:29 -0400

    Finished Message Component

--- client/src/components/Message/Message.js
+++ client/src/components/Message/Message.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Message.css';
 
-const Message = ({ message, name }) => {
+const Message = ({ message: {user, text}, name }) => {
     let isSentByCurrentUser = false;
     const trimmedName = name.trim().toLowerCase();
     if(user === trimmedName) {
@@ -9,7 +9,23 @@ const Message = ({ message, name }) => {
     }
 
     return (
-        isSentByCurrentUser ? () : ;
+        isSentByCurrentUser ? (
+            <div className="messageContainer justifyEnd">
+                <p className="sentText pr-10">{trimmedName}</p>
+                <div className="messageBox backgroundBlue">
+                    <p classname="messageText colorWhite">{text}</p>
+                </div>
+            </div>
+        ) 
+        
+        : (
+            <div className="messageContainer justifyStart">
+                <div className="messageBox backgroundLight">
+                    <p classname="messageText colorDark">{text}</p>
+                </div>
+                <p className="sentText pl-10">{user}</p>
+            </div>
+        )
     )
 }
 
--- client/src/components/Messages/Messages.js
+++ client/src/components/Messages/Messages.js
@@ -3,10 +3,10 @@ import './Messages.css';
 import Message from '../../components/Message/Message'
 import ScrollToBottom from 'react-scroll-to-bottom';
 
-const Messages = ({messages, name}) => {
+const Messages = ({messages, name}) => (
     <ScrollToBottom>
         {messages.map((message, i) => <div key={i}><Message message={message} name={name} /></div>)}
     </ScrollToBottom>
-}
+)
 
 export default Messages;
