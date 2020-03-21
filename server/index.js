6b928e6cc15f5ff210b259340868ae7d77482cc0
ZachJohn64 <zachdjohnson64@gmail.com>
2020-03-20 21:22:57 -0400

    Reverted back to before heroku deployment

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const router = require('./router')
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server);
-const path = require('path');
+
 const cors = require('cors')
 
 io.on('connection', (socket) => {
@@ -46,14 +46,6 @@ io.on('connection', (socket) => {
 })
 
 app.use(router);
-// Serve static assets if in production
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-}
 app.use(cors());
 
 server.listen(PORT, () => console.log(`Server has started on port ${PORT}`));
